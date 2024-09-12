8fd0a4a9a91aca6fc170d9d5fabf40d63095e62f
@gsrd2803 <gsrd2803@gmail.com>
2024-09-11 23:05:51 -0400

    Configurar Flask para servir archivos estáticos de React

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,10 @@
-from flask import Flask, jsonify
+import os
+from flask import Flask, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../frontend/build', static_url_path='/')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'your_secret_key'
@@ -13,8 +14,8 @@ bcrypt = Bcrypt(app)
 CORS(app)
 
 @app.route('/')
-def home():
-    return jsonify(message="Hello, Flask!")
+def serve():
+    return send_from_directory(app.static_folder, 'index.html')
 
 from backend import routes
 
