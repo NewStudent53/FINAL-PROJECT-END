19221ec564c66e635c80bf9a36e3164c300debee
@gsrd2803 <gsrd2803@gmail.com>
2024-09-11 23:12:12 -0400

    Version anterior

--- backend/app.py
+++ backend/app.py
@@ -1,21 +1,14 @@
-import os
-from flask import Flask, send_from_directory
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
-
-app = Flask(__name__, static_folder='../frontend/build', static_url_path='/')
+app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'your_secret_key'
-
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 CORS(app)
-
-@app.route('/')
-def serve():
-    return send_from_directory(app.static_folder, 'index.html')
 
 from backend import routes
 
